7ecbcd4443e27057ce447521636269c091a76514
Alvaro Morales <amanuel@intermexusa.com>
2026-07-23 00:24:04 -0300

    Configure Firebase Hosting and PWA

---
+++ src/firebase/config.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD1PWVQCLSlzP4R9tOyEHz4nIrFYvGQz6o",
+  authDomain: "libreta-digital-3f11f.firebaseapp.com",
+  projectId: "libreta-digital-3f11f",
+  storageBucket: "libreta-digital-3f11f.firebasestorage.app",
+  messagingSenderId: "962698597432",
+  appId: "1:962698597432:web:5a50f1baf0af1aba8442cd",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
