75970a1e099b9140e0a7cb0bc3ef12cdff5ae3d6
Alvaro Morales <amanuel@intermexusa.com>
2026-08-13 16:44:53 -0300

    refactor: add stable product identifiers

--- src/data/products.ts
+++ src/data/products.ts
@@ -3,42 +3,42 @@ import { Product } from "../models/Product";
 export const products: Product[] = [
 
   {
-    id:1,
-    name:"Coca Cola 600",
-    price:55,
-    active:true,
+    id: "coca-cola-600",
+    name: "Coca Cola 600",
+    price: 55,
+    active: true,
     category: "Bebidas",
   },
 
   {
-    id:2,
-    name:"Sprite 600",
-    price:55,
-    active:true,
+    id: "sprite-600",
+    name: "Sprite 600",
+    price: 55,
+    active: true,
     category: "Bebidas",
   },
 
   {
-    id:3,
-    name:"Leche Conaprole",
-    price:52,
-    active:true,
+    id: "leche-conaprole",
+    name: "Leche Conaprole",
+    price: 52,
+    active: true,
     category: "Lácteos",
   },
 
   {
-    id:4,
-    name:"Pan flauta",
-    price:65,
-    active:true,
+    id: "pan-flauta",
+    name: "Pan flauta",
+    price: 65,
+    active: true,
     category: "Panadería",
   },
 
   {
-    id:5,
-    name:"Yerba Canarias 1kg",
-    price:265,
-    active:true,
+    id: "yerba-canarias-1kg",
+    name: "Yerba Canarias 1kg",
+    price: 265,
+    active: true,
     category: "Té y Café",
   },
 
